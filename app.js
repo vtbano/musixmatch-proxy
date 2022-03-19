781c09032be0e9aebfb372ac4ddbb1558d44f7b9
SlimTim10 <slimtim10@gmail.com>
2022-03-19 08:23:06 -0400

    Set up proxy server for musixmatch API

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const axios = require('axios')
+const hostWhitelist = require('./hostWhitelist.json')
+const app = express()
+const port = process.env.PROXY_PORT || 80
+const apikey = process.env.MUSIXMATCH_API_KEY
+
+const restrictHost = (req, res, next) => {
+  const host = req.get('host')
+  if (hostWhitelist.includes(host)) {
+    next()
+  } else {
+    res.send('No access')
+  }
+}
+
+app.use(restrictHost)
+
+app.get('/chart.tracks.get', async (req, res) => {
+  const apiResponse = await axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apikey}`);
+  res.json(apiResponse.data.message.body)
+})
+
+app.get('/matcher.lyrics.get', async (req, res) => {
+  const apiResponse = await axios.get(`https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${req.query.q_track}&q_artist=${req.query.q_artist}&apikey=${apikey}`);
+  res.json(apiResponse.data.message.body)
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
