af626f9ed1cb8319d6a601c80a07ca73f1aa6797
vtbano <92877260+vtbano@users.noreply.github.com>
2022-03-21 16:08:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
-const hostWhitelist = require('./whitelist.json')
+const whitelist = require('./whitelist.json')
 const app = express()
 const port = process.env.PORT || 80
 const apikey = process.env.MUSIXMATCH_API_KEY
 
-const restrictHost = (req, res, next) => {
-  console.log('origin:', req.get('origin'))
-  console.log('referer:', req.get('referer'))
-  const host = req.get('origin')
-  console.log('Incoming request from host:', host)
-  if (hostWhitelist.includes(host)) {
+const restriction = (req, res, next) => {
+  const origin = req.get('origin')
+  const ip = req.ip
+  console.log('Incoming request origin:', origin)
+  console.log('Incoming request ip:', ip)
+  if (whitelist.includes(origin) || whitelist.includes(ip)) {
     next()
   } else {
     res.send('No access')
@@ -20,7 +20,7 @@ const restrictHost = (req, res, next) => {
 
 app.use(cors())
 
-app.use(restrictHost)
+app.use(restriction)
 
 app.get('/chart.tracks.get', async (req, res) => {
   const apiResponse = await axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=50&country=US&f_has_lyrics=1&apikey=${apikey}`);
